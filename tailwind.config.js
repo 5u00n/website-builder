813ba3a2f859465583b803e003336c71f825dd0a
5u00n <surenhembram97@gmail.com>
2024-10-20 15:47:04 +0530

    fixing tailwind and converting to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Paths to your source files
+    "./src/**/*.{jsx,tsx}",
+    "./src/**/**/*.{jsx,tsx}",
+    "./public/**/*.{jsx,tsx}",
   ],
   theme: {
     extend: {},
